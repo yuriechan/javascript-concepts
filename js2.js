bda9f505621d2a53ea3c2f000b79cc7ca8e913e6
max-d-w <mwerman95@gmail.com>
2019-09-06 12:10:08 +0300

    Completed functions

--- js2.js
+++ js2.js
@@ -3,8 +3,12 @@ Create a function called myFunctionOne that returns the sum of all values of an 
 */
 
 //Concept 1
-function myFunctionOne() {
-
+function myFunctionOne(arr) {
+    var sum = 0;
+    arr.forEach(element => {
+       sum += element; 
+    });
+    return sum;
 }
 
 
@@ -13,7 +17,13 @@ Create a function called myFunctionTwo with one parameter that counts the number
 */
 
 //Concept 2
-
+function myFunctionTwo(arr){
+    var count = 0;
+    arr.forEach(element => {
+        count ++;
+    });
+    return count;
+}
 
 /* Concept 3 - Nested Loops
 Create a function called myFunctionThree with one parameter that loops through each string in an array and adds each character
@@ -25,13 +35,26 @@ Return: 'onetwothree'
 */
 
 //Concept 3
-
+function myFunctionThree(arr){
+    var result = "";
+    arr.forEach(element => {
+        result = result.concat(element);
+    });
+    return result;
+}
 
 /* Concept 4 - Arrays
 Create a function called myFunctionFour that uses a loop to push all numbers from 1 to 100 into an array called 'numArray'.
 */
 
 //Concept 4
+function myFunctionFour(){
+    var numArray = [];
+    for(i = 1; i<101; i++){
+        numArray.push(i);
+    }
+    return numArray;
+}
 
 
 /* Concept 5 - Arrays 2
@@ -44,6 +67,15 @@ Return: [2, 4, 6]
 */
 
 //Concept 5
+function myFunctionFive(arr){
+    var newArray = [];
+    arr.forEach(element => {
+        if (element%2 == 0){
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
 
 
 
