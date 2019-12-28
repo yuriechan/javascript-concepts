6c851d4f0d840249f74911e325bb7e0b99ddf801
Yurie <cutecupcakeandcute@gmail.com>
2019-12-28 17:40:48 +0900

    delete all the answers to each question

--- js1.js
+++ js1.js
@@ -8,11 +8,7 @@ t4 - array
 */
 
 //Concept 1
-var x = 0
-var t1 = 1;
-var t2 = "string";
-var t3 = true;
-var t4 = [1,2];
+
 
 /*
 Concept 2 - Simple Function Returns
@@ -20,9 +16,7 @@ Create a function named myFunctionOne that returns the number 3.
 */
 
 //Concept 2
-function myFunctionOne() {
-    return 3;
-}
+
 
 /*
 Concept 3 - Parameters 1
@@ -30,9 +24,7 @@ Create a function named myFunctionTwo with two parameters that returns their sum
 */
 
 //Concept 3
-function myFunctionTwo(num1, num2){
-    return num1 + num2;
-}
+
 
 /*
 Concept 4 - Parameters 2
@@ -40,9 +32,7 @@ Create a function named myFunctionThree with three parameters that returns all o
 */
 
 //Concept 4
-function myFunctionThree(num1, num2, num3){
-    return num1*num2*num3;
-}
+
 
 /*
 Concept 5 - Parameters 3
@@ -50,13 +40,7 @@ Create a function named myFunctionFour with two parameters that returns their su
 */
 
 //Concept 5
-function myFunctionFour(num1, num2){
-    // Direction slightly unclear, assuming that if either is zero the return value should be false
-    if (num1*num2 === 0){
-        return false;
-    }
-    return num1+num2;
-}
+
 
 
 //TESTING FUNCTIONS - DO NOT TOUCH ANYTHING BELOW THIS POINT
