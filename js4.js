0ac818add502fd2bf33ebdac63c800e427813b65
Yurie <cutecupcakeandcute@gmail.com>
2019-12-29 14:39:12 +0900

    complete js.4

--- js4.js
+++ js4.js
@@ -7,11 +7,18 @@
 //forEach by taking in an array and a function that is invoked on each
 //element of that array and returns the values. Do not use forEach() or map().
 
-
+function onEvery(arr, func){
+    for (let i = 0; i < arr.length; i++) {
+         arr[i] = func(arr[i])
+    }
+    return arr
+}
 
 //Create a function that takes in a number and an infinite number of 
 //functions, runs all the functions on the single number, then returns it.
 
-
+function marathon(num, funcOne, funcTwo, funcThree){
+    return funcThree(funcTwo(funcOne(num)))
+}
 
 module.exports = { onEvery, marathon }
